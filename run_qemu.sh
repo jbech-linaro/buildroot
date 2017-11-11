fe00c1a17a7cf0d2ae32898ab1266df76c847a20
Joakim Bech <joakim.bech@linaro.org>
2017-11-11 12:23:12 +0100

    Add QEMU script based on OP-TEE kernel
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

---
+++ run_qemu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+QEMU=/mnt/sshd/devel/optee_projects/qemu/qemu/arm-softmmu/qemu-system-arm
+
+$QEMU -machine virt -nographic \
+	-kernel /mnt/sshd/devel/optee_projects/qemu/linux/arch/arm/boot/zImage \
+	-append "console=ttyAMA0" \
+	-initrd output/images/rootfs.cpio.gz \
+	-netdev user,id=vmnic -device virtio-net-device,netdev=vmnic
