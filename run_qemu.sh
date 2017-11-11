891b571dfd6a5138e7a17e427011ca35ae6a7538
Joakim Bech <joakim.bech@linaro.org>
2017-11-11 12:26:19 +0100

    Add network config
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- run_qemu.sh
+++ run_qemu.sh
@@ -5,4 +5,5 @@ $QEMU -machine virt -nographic \
 	-kernel /mnt/sshd/devel/optee_projects/qemu/linux/arch/arm/boot/zImage \
 	-append "console=ttyAMA0" \
 	-initrd output/images/rootfs.cpio.gz \
-	-netdev user,id=vmnic -device virtio-net-device,netdev=vmnic
+	-netdev user,id=vmnic -device virtio-net-device,netdev=vmnic \
+	-redir tcp:2222::22
